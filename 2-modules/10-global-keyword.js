beccd68f6d18a57fdb4eeb558bacb001e18c59f4
quintero git <hello.quinteroo@gmail.com>
2025-04-02 20:49:01 +0200

    UPDATE 2-modules

--- 2-modules/10-global-keyword.js
+++ 2-modules/10-global-keyword.js
@@ -1,28 +1,28 @@
 
 
 
-! VARIABLE GLOBAL
-es una variable que contiene todas las funciones, objetos, variables... de nuestra aplicación.
+// ! VARIABLE GLOBAL
+// es una variable que contiene todas las funciones, objetos, variables... de nuestra aplicación.
 
-En Node.js, la palabra clave global hace referencia a un objeto que contiene todas las variables, funciones y 
-objetos disponibles en todo el entorno de ejecución de Node.js.
+// En Node.js, la palabra clave global hace referencia a un objeto que contiene todas las variables, funciones y
+// objetos disponibles en todo el entorno de ejecución de Node.js.
 
-global es similar al objeto window en el navegador, pero se usa en el entorno de Node.js. Puedes acceder a objetos o 
-variables definidas globalmente en cualquier parte del código.
+// global es similar al objeto window en el navegador, pero se usa en el entorno de Node.js. Puedes acceder a objetos o
+// variables definidas globalmente en cualquier parte del código.
 
-Ejemplo:
-global.miVariable = "Soy global!";
-console.log(global.miVariable); // "Soy global!"
+// Ejemplo:
+// global.miVariable = "Soy global!";
+// console.log(global.miVariable); // "Soy global!"
 
-------------------------------------------------------------------
+// ------------------------------------------------------------------
 
-Es decir, tenemos dos entornos de ejecución, uno el navegaor y otro el servidor,
-en JS tenemos la variable globalThis, en el navegador se interpreta como window y en el servidor como global.
+// Es decir, tenemos dos entornos de ejecución, uno el navegaor y otro el servidor,
+// en JS tenemos la variable globalThis, en el navegador se interpreta como window y en el servidor como global.
 
-De hecho, muchas de las herramientas que usamos (fetch, console...) son propiedades de globalthis: globalThis.console.log("hola")
+// De hecho, muchas de las herramientas que usamos (fetch, console...) son propiedades de globalthis: globalThis.console.log("hola")
 
-------------------------------------------------------------------
+// ------------------------------------------------------------------
 
 
-Sin embargo, en Node.js es recomendable evitar el uso de global a menos que sea necesario, 
-ya que puede hacer que el código sea más difícil de depurar y mantener.
+// Sin embargo, en Node.js es recomendable evitar el uso de global a menos que sea necesario,
+// ya que puede hacer que el código sea más difícil de depurar y mantener.
---
+++ 2-modules/11-native-modules.js
@@ -0,0 +1,10 @@
+
+
+
+//! 🔹 MÓDULOS NATIVOS DE NODE.JS
+// Los módulos nativos de Node.js son paquetes integrados en Node que no 
+// necesitan instalación con npm. Están listos para usarse en cualquier aplicación y permiten 
+// interactuar con el sistema operativo, manejar archivos, crear servidores, trabajar con eventos y más.
+
+const fs = require("node:fs") // desde node 16 se recomienda -->node:
+
