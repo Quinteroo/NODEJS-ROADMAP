d49eb101c3cfc8c44f690164c0d3b6bca73972eb
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-02 16:42:07 +0200

    Update 8-modules.js

--- 8-modules.js
+++ 8-modules.js
@@ -14,8 +14,18 @@ Node.js admite dos sistemas de módulos: CommonJS y ESM (ECMAScript Modules).
 !🔹 1. CommonJS (CJS)
 CommonJS es el sistema de módulos tradicional que se usa en Node.js desde sus primeros días. Funciona de la siguiente manera:
 
+!Características de CommonJS:
+*Sincronización: 
+require es sincrónico, lo que significa que se espera a que se cargue el módulo antes de seguir ejecutando el código.
+
+
+  
+*Cargando módulos: 
+Cuando usas require, Node.js lee y ejecuta el módulo en el momento de la importación.
+
 *Exportar módulos: 
 En CommonJS, usamos module.exports o exports para exportar funciones, objetos o variables de un archivo. 
+Exportarlo de esta forma nos permite renombrar la función.
 // ejemplo.js
 const saludo = "Hola, mundo!";
 module.exports = saludo;
@@ -27,51 +37,22 @@ Para importar un módulo, se utiliza require.
 const saludo = require('./ejemplo');
 console.log(saludo); // "Hola, mundo!"
 
+// renombra la función
+const unSaludoMiGente = require('./ejemplo')
+console.log(unSaludoMiGente)
 
-!Características de CommonJS:
-*Sincronización: 
-require es sincrónico, lo que significa que se espera a que se cargue el módulo antes de seguir ejecutando el código.
-
-*Cargando módulos: 
-Cuando usas require, Node.js lee y ejecuta el módulo en el momento de la importación.
-
-
-
-!2. ESM (ECMAScript Modules)
-ESM es el sistema de módulos oficial y moderno de JavaScript, basado en el estándar ECMAScript. Es más familiar para 
-los desarrolladores que trabajan tanto con Node.js como con navegadores web, ya que es el mismo sistema que se utiliza en el frontend.
-
-  
-*Exportar módulos: 
-En ESM, usamos export para exportar funciones, clases, objetos o variables.
-// ejemplo.mjs
+------------------------------------------------------------------------------------------------
+Para "obligar" a conservar el nombrem podemos pasarlo como un objeto y al importartlo hacer destructuring
+// ejemplo.js
 const saludo = "Hola, mundo!";
-export default saludo;
+module.exports = {
+  saludo
+  {
 
-
-*Importar módulos: 
-Para importar un módulo en ESM, usamos import.
-// app.mjs
-import saludo from './ejemplo.mjs';
+// app.js
+const {saludo{ = require('./ejemplo');
 console.log(saludo); // "Hola, mundo!"
-Características de ESM:
 
-
-*Asíncrono: 
-import es asíncrono, lo que significa que permite optimizaciones como la carga diferida.
-
-  
-*Soporte para importación/ exportación dinámica: 
-Puedes usar import() de manera dinámica.
-
-  
-!¿Cómo usar ESM en Node.js? 
-Para usar ESM en Node.js, debes asegurarte de que el archivo tenga la extensión .mjs o declarar en el package.json "type": "module".
-  
-// package.json
-{
-  "type": "module"
-}
 
 
   
