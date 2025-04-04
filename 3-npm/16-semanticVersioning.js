902bebbc640b5e5e3c2781dbfae3747cf7795414
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-04 16:26:09 +0200

    Create 16-semanticVersioning.js

---
+++ 3-npm/16-semanticVersioning.js
@@ -0,0 +1,25 @@
+
+
+
+SEMANTIC VERSIONING
+Semantic versioning (versionado semántico) es un sistema de numeración de versiones que ayuda a entender qué tipo de 
+  cambios se han hecho en una librería, paquete o software, con solo ver su número de versión.
+
+El formato general es:
+
+MAJOR.MINOR.PATCH
+por ejemplo: 2.4.1
+
+Esto significa:
+
+MAJOR (versión mayor): 
+se incrementa cuando hay cambios incompatibles que pueden romper el código que depende de esa librería.
+Ejemplo: 1.0.0 → 2.0.0 (se cambió algo que ya no funciona igual que antes)
+
+MINOR (versión menor): 
+se incrementa cuando se añade funcionalidad nueva sin romper lo que ya había.
+Ejemplo: 1.2.0 → 1.3.0
+
+PATCH (parche): 
+se incrementa cuando se corrige un error o bug sin añadir funcionalidades nuevas ni romper lo anterior.
+Ejemplo: 1.3.2 → 1.3.3
